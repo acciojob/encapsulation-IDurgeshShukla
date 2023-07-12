b567cdc2dd1a3c1cf4ba4f8bb8cb10eda1e883d1
IDurgeshShukla <shukladurgesh4545@gmail.com>
2023-07-12 20:17:17 +0530

    changes done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+       // obj.var;
+        // Error 'var' has private access in RWOnly.java
+        int oldvalue = obj.getVar();
+        System.out.println(oldvalue);
+        int updated = obj.setVar(50);
+        System.out.println(updated);
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int var = 10;
+    public int getVar(){
+        return var;
+    }
+    public int setVar(int val){
+        this.var = val;
+        return val;
+    }
 }
